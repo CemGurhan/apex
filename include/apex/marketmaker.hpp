7e1adee7eb728f8b44fd8735b91426a37370ae18
cemelwood <cem.gurhan@elwood.io>
2026-04-26 17:31:28 +0100

    market maker class

--- include/apex/marketmaker.hpp
+++ include/apex/marketmaker.hpp
@@ -1,5 +1,11 @@
 #include "exchange.hpp"
+#include <functional>
+#include <unordered_set>
 
 class MarketMaker {
-
+    private:
+        Exchange& exchange;
+        int64_t inventory = 0;
+        std::unordered_set<uint64_t> active_order_ids; 
+        int64_t target_spread;
 };
