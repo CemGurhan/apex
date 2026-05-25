e347e1e5a9758a663717f98ca1af1f9e4ef85da2
cemelwood <cem.gurhan@elwood.io>
2026-05-25 01:36:53 +0100

    reduce sim time to 30 secs

--- app/sim/sim.hpp
+++ app/sim/sim.hpp
@@ -5,4 +5,4 @@
 // runSim wires up a simulated feeder against an orderbook and a market maker,
 // then lets the feeder run for the given duration before tearing everything down.
 // Entry point for the --sim CLI flag.
-void runSim(std::chrono::seconds duration = std::chrono::minutes(1));
+void runSim(std::chrono::seconds duration = std::chrono::seconds(30));
